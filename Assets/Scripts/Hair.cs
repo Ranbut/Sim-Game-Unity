96ca21ffbb209a4b796e18b8b570332ef709ec36
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-16 11:37:22 -0300

    Added ScriptableObject Hair and 16 male hairs

---
+++ Assets/Scripts/Hair.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Hair", menuName = "Hair")]
+public class Hair : ScriptableObject
+{
+    public Sprite frontSprite;
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+    public Sprite backSprite;
+}
