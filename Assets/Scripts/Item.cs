73b6af9282bb0d340ae0757de964ffe0e5556e07
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-16 16:48:04 -0300

    Updated Project Preferences

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Item : ScriptableObject
 {
+    [Header("Item")]
     public new string name;
     public string description;
+    [Space(5)]
+    [Header("Value")]
     public int buyValue;
     public int sellValue;
 }
--- Assets/Scripts/Shirt.cs
+++ Assets/Scripts/Shirt.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Shirt", menuName = "Item/Shirt")]
 public class Shirt : Item
 {
+    [Space(5)]
+    [Header("Shirt")]
     public Sprite frontSprite;
     public Sprite rightSprite;
     public Sprite leftSprite;
