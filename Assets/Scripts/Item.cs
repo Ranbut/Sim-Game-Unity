fa30821f54fb6751e494c4131e3484b9036ca9bd
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-16 11:18:07 -0300

    Added ScriptableObject Shirt and 10 shirts

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public int buyValue;
+    public int sellValue;
+}
---
+++ Assets/Scripts/Shirt.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Shirt", menuName = "Item/Shirt")]
+public class Shirt : Item
+{
+    public Sprite frontSprite;
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+    public Sprite backSprite;
+}
